6af23da9c7a00a3d0c682393817b574c5debb04b
Bassel-T <lessab99@gmail.com>
2023-04-24 18:53:31 -0500

    Repush

--- VoronoiProject/Models/Face.cs
+++ VoronoiProject/Models/Face.cs
@@ -3,6 +3,6 @@
 	/// A very simple Face model, storing the first edge that contains it
 	/// </summary>
 	public class Face {
-		public Edge Edge { get; set; }
+		public Edge? Edge { get; set; } = null;
 	}
 }
